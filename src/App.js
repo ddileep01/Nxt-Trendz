4d662ecd0816e3e0d5af39a61137c94edccb17f2
dileep-doddi <dileep18052001@gmail.com>
2022-11-22 08:05:31 +0000

    Get Exclusive Prime Deals code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
@@ -10,13 +10,16 @@ import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
 const App = () => (
-  <Switch>
-    <Route exact path="/login" component={LoginForm} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/products" component={Products} />
-    <ProtectedRoute exact path="/cart" component={Cart} />
-    <Route component={NotFound} />
-  </Switch>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/products" component={Products} />
+      <ProtectedRoute exact path="/cart" component={Cart} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="not-found" />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
